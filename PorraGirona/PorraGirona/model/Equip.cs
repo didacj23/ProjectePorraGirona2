574171ce8200708eb55a11f246168cd26dd14ee7
Dídac <a2207807@institutmontilivi.cat>
2023-05-15 13:28:56 +0200

    classes equip i partit

---
+++ PorraGirona/PorraGirona/model/Equip.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorraGirona
+{
+    internal class Equip
+    {
+        private string nom;
+        private string camp;
+        private string foto;
+        private string categoria;
+        private int totalPartitsGuanyats;
+        private int totalPartitsPerduts;
+        private int totalPartitsEmpatats;
+        private LlistaJugadors jugadors;
+
+
+        public Equip(string nom, string camp, string foto, string categoria, LlistaJugadors jugadors)
+        {
+            this.nom = nom;
+            this.camp = camp;
+            this.foto = foto;
+            this.categoria = categoria;
+            totalPartitsGuanyats=0;
+            totalPartitsPerduts=0;
+            totalPartitsEmpatats=0;
+            this.jugadors=jugadors;
+        }
+
+
+
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public string Camp
+        {
+            get { return camp; }
+            set { camp = value; }
+        }
+
+        public string Foto
+        {
+            get { return foto; }
+            set { foto = value; }
+        }
+
+        public string Categoria
+        {
+            get { return categoria; }
+            set { categoria = value; }
+        }
+
+        public int TotalPartitsGuanyats
+        {
+            get { return totalPartitsGuanyats;}
+            set {  totalPartitsGuanyats = value; }
+        }
+
+        public int TotalPartitsPerduts
+        {
+            get { return totalPartitsPerduts;}
+            set { totalPartitsPerduts = value;}
+        }
+
+        public int TotalPartitsEmpatats
+        {
+            get { return  totalPartitsEmpatats; }
+            set { totalPartitsEmpatats = value; }
+        }
+
+        public LlistaJugadors Jugadors
+        {
+            get { return jugadors; }
+            set { jugadors=value; }
+        }
+
+        
+
+        public string ToString()
+        {
+            return $"Nom: {Nom} \n"+
+                    $"Camp: {Camp} \n"+
+                    $"Foto: {Foto} \n" +
+                    $"Categoria: {Categoria} \n" +
+                    $"TotalPartitsGuanyats: {TotalPartitsGuanyats} \n" +
+                    $"TotalPartitsPerduts: {TotalPartitsPerduts} \n" +
+                    $"TotalPartitsEmpatats: {TotalPartitsEmpatats} \n" +
+                    $"Jugadors: {Jugadors}";
+        }
+
+    }
+}
---
+++ PorraGirona/PorraGirona/model/Partit.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorraGirona
+{
+    internal class Partit
+    {
+        private static int contPartit=0;
+        private int idPartit;
+        private Equip equipA;
+        private Equip equipB;
+        private int golsEquipA;
+        private int golsEquipB;
+        private DateTime diaHora;
+        private string temporada;
+        private string camp;
+        private string estat;
+
+        public Partit(Equip equipA, Equip equipB, DateTime diaHora, string temporada, string camp)
+        {
+            idPartit=contPartit;
+            contPartit++;
+
+            this.equipA = equipA;
+            this.equipB = equipB;
+            golsEquipA=0;
+            golsEquipB=0;
+            this.diaHora=diaHora;
+            this.temporada=temporada;
+            this.camp=camp;
+            estat="programat";
+        }
+
+        public int ContPartit
+        {
+            get { return contPartit;}
+        }
+
+        public int IdPartit
+        {
+            get { return idPartit; }
+        }
+
+        public Equip EquipA
+        {
+            get { return equipA; }
+        }
+
+        public Equip EquipB
+        {
+            get { return equipB; }
+        }
+
+        public int GolsEquipA
+        {
+            get { return golsEquipA; }
+            set { golsEquipA=value; }
+        }
+
+        public int GolsEquipB
+        {
+            get { return golsEquipB; }
+            set { golsEquipB = value; }
+        }
+
+        public DateTime DiaHora
+        {
+            get { return diaHora; }
+            set { diaHora = value; }
+        }
+
+        public string Temporada
+        {
+            get { return temporada; }
+            set { temporada=value; }
+        }
+
+        public string Camp
+        {
+            get { return camp; }
+            set { camp=value; }
+        }
+
+        public string Estat
+        {
+            get { return estat; }
+            set { estat=value; }}
+        }
+
+        /*public override string ToString()
+        {
+            return
+        }*/
+
+    }
+}
