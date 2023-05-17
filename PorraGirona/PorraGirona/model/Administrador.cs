acea577335bc325d31d33673d75e55bfcf63b2a7
Dídac <a2207807@institutmontilivi.cat>
2023-05-17 11:48:43 +0200

    pantalles inici usuari, pronostic, classe administrador

---
+++ PorraGirona/PorraGirona/model/Administrador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PorraGirona.model
+{
+    internal class Administrador:Usuari
+    {
+        public Administrador(string nom, string cognom, string dni):base(nom, cognom, dni) { }
+
+        public bool CrearJugador(string dni, string nom, string cognom, Equip equip, string posicio, int numero)
+        {
+            
+            Jugador j = new Jugador(dni, nom, cognom, equip, posicio, numero);
+
+            return true;
+        }
+    }
+}
