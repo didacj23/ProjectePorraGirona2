7a2e1ed4f76b368879a532e89b1ac6ee57da3147
Arnau Gispert <arnau.gispert.coll@gmail.com>
2023-05-24 09:31:49 +0200

    Finalitzar entorn
    Entorn  finalitzat

--- PorraGirona/PorraGirona/usuari/Administrador.xaml.cs
+++ PorraGirona/PorraGirona/usuari/Administrador.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -57,6 +59,36 @@ namespace PorraGirona.usuari
             inp_id_admin.Visibility = rad_programarPartit_admin.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
 
             btn_ProgramarPartit_admin.Visibility = rad_programarPartit_admin.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void rad_CrearEquip_admin_Checked(object sender, RoutedEventArgs e)
+        {
+            lbl_NomCamp_admin.Visibility = rad_CrearEquip_admin.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            lbl_NomEquip_admin.Visibility = rad_CrearEquip_admin.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+
+            inp_NomCamp_admin.Visibility = rad_CrearEquip_admin.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+            inp_NomEquip_admin.Visibility = rad_CrearEquip_admin.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+
+            btn_PujarFoto_admin.Visibility = rad_CrearEquip_admin.IsChecked == true ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void btn_PujarFoto_admin_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de imagen (*.jpg, *.jpeg, *.png)|*.jpg;*.jpeg;*.png";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string UsuariPath = openFileDialog.FileName;
+                string destinationDirectory = "usuari/logos";
+                string NomLogo = new FileInfo(UsuariPath).Name;
+
+                string projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                string CarpetaLogos = System.IO.Path.Combine(projectDirectory, destinationDirectory, NomLogo);
+
+                File.Copy(UsuariPath, CarpetaLogos, true);
+
+            }
         }
     }
 }
